d7df4449b11e04adf91a5a21784bc8c4c0856494
alicelifogarty <AliceLi@Alices-MacBook-Pro-2.local>
2016-07-11 14:30:18 -0400

    Cleaning up Wedding, CCRH, VBMS

--- js/main.js
+++ js/main.js
@@ -2,17 +2,50 @@
 
 // on click of leftBigPic, rightTopPic, rightBtmPic enlarge to whole screen
 
-// Scroll to Top
+$( document ).ready(function() {
 
-jQuery(window).scroll(function() {
-    if (jQuery(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        jQuery('#return-to-top').fadeIn(200);    // Fade in the arrow
-    } else {
-        jQuery('#return-to-top').fadeOut(200);   // Else fade out the arrow
-    }
+        'use strict';
+
+        var smoothStateOptions = {
+            prefetch: true,
+            cacheLength: 2,
+            anchors: 'a',
+            onStart: {
+                duration: 2400, // Duration of our animation
+                render: function ($container) {
+                    // Add your CSS animation reversing class
+                    $container.addClass('is-exiting');
+
+                    // Restart your animation
+                    //smoothState.restartCSSAnimations();
+                }
+            },
+            onReady: {
+                duration: 0,
+                render: function ($container, $newContent) {
+                    // Remove your CSS animation reversing class
+                    $container.removeClass('is-exiting');
+
+                    // Inject the new content
+                    $container.html($newContent);
+                    $container.addClass('is-entering');
+
+                }
+            },
+            onAfter: {
+                duration: 0,
+                render: function ($container, $newContent) {
+                    // Remove your CSS animation reversing class
+                    $container.removeClass('is-entering');
+
+
+                }
+            }
+        };
+
+        var smoothState = $('#main').smoothState(smoothStateOptions);
+
+    //$('#main').smoothState({ debug: true });
+
 });
-jQuery('#return-to-top').click(function() {      // When arrow is clicked
-    jQuery('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
-    }, 500);
-});
+
