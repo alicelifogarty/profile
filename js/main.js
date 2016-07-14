ec349b9ec92b155ca77f2eeb889280b94f9f8b87
alicelifogarty <AliceLi@Alices-MacBook-Pro-2.local>
2016-07-14 19:49:08 -0400

    Added JS for return-to-top

--- js/main.js
+++ js/main.js
@@ -9,6 +9,19 @@ jQuery( document ).ready(function() {
             closeEffect : 'none'
     });
 
+    $(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
+
     'use strict';
 
         var smoothStateOptions = {
