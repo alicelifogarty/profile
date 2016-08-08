44e4a16f7fb67471a4765a6dc9d7499d3c5dc067
alicelifogarty <alicelifogarty@gmail.com>
2016-08-08 14:15:52 -0400

    Turning off smoothstate

--- js/main.js
+++ js/main.js
@@ -15,53 +15,53 @@ jQuery( document ).ready(function() {
     } else {
         $('#return-to-top').fadeOut(200);   // Else fade out the arrow
     }
-});
+    });
 $('#return-to-top').click(function() {      // When arrow is clicked
     $('body,html').animate({
         scrollTop : 0                       // Scroll to top of body
     }, 500);
-});
+    });
 
-    'use strict';
+    // 'use strict';
 
-        var smoothStateOptions = {
-            prefetch: true,
-            cacheLength: 2,
-            anchors: '.mainWork a, .movingNav a', 
-            onStart: {
-                duration: 2400, // Duration of our animation
-                render: function ($container) {
-                    // Add your CSS animation reversing class
-                    $container.addClass('is-exiting');
+    //     var smoothStateOptions = {
+    //         prefetch: true,
+    //         cacheLength: 2,
+    //         anchors: '.mainWork a, .movingNav a', 
+    //         onStart: {
+    //             duration: 2400, // Duration of our animation
+    //             render: function ($container) {
+    //                 // Add your CSS animation reversing class
+    //                 $container.addClass('is-exiting');
 
-                    // Restart your animation
-                    //smoothState.restartCSSAnimations();
-                }
-            },
-            onReady: {
-                duration: 0,
-                render: function ($container, $newContent) {
-                    // Remove your CSS animation reversing class
-                    $container.removeClass('is-exiting');
+    //                 // Restart your animation
+    //                 //smoothState.restartCSSAnimations();
+    //             }
+    //         },
+    //         onReady: {
+    //             duration: 0,
+    //             render: function ($container, $newContent) {
+    //                 // Remove your CSS animation reversing class
+    //                 $container.removeClass('is-exiting');
 
-                    // Inject the new content
-                    $container.html($newContent);
-                    $container.addClass('is-entering');
+    //                 // Inject the new content
+    //                 $container.html($newContent);
+    //                 $container.addClass('is-entering');
 
-                }
-            },
-            onAfter: {
-                duration: 0,
-                render: function ($container, $newContent) {
-                    // Remove your CSS animation reversing class
-                    $container.removeClass('is-entering');
+    //             }
+    //         },
+    //         onAfter: {
+    //             duration: 0,
+    //             render: function ($container, $newContent) {
+    //                 // Remove your CSS animation reversing class
+    //                 $container.removeClass('is-entering');
 
 
-                }
-            }
-        };
+    //             }
+    //         }
+    //     };
 
-        var smoothState = $('#main').smoothState(smoothStateOptions);
+    //     var smoothState = $('#main').smoothState(smoothStateOptions);
 
     //$('#main').smoothState({ debug: true });
 
