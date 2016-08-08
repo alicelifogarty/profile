389aad9040858339c9d68f0c1d3f4729edfabf32
alicelifogarty <alicelifogarty@gmail.com>
2016-08-08 16:27:13 -0400

    more wow

--- js/main.js
+++ js/main.js
@@ -38,6 +38,17 @@ $('#return-to-top').click(function() {      // When arrow is clicked
 
     new WOW().init();
 
+     wow = new WOW(
+    {
+        boxClass:     'wow',      // default
+        animateClass: 'animated', // change this if you are not using animate.css
+        offset:       0,          // default
+        mobile:       true,       // keep it on mobile
+        live:         true        // track if element updates
+      }
+    )
+   wow.init();
+
     // 'use strict';
 
     //     var smoothStateOptions = {
