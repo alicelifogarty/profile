a58ff8daf6126fe2d45c162d495b53c55f06c055
alicelifogarty <alicelifogarty@gmail.com>
2016-09-04 20:06:40 -0400

    added smooth scrolling to links

--- js/main.js
+++ js/main.js
@@ -83,5 +83,20 @@ $('#return-to-top').click(function() {      // When arrow is clicked
 
     //$('#main').smoothState({ debug: true });
 
+    $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
 });
 
